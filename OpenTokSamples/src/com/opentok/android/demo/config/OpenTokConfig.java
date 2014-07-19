6473e422a6e088d905dab425c53ff10f60bed943
Luca De Franceschi <luca.defranceschi.91@gmail.com>
2014-07-19 15:40:33 +0200

    Changed version compatibility to 15+

--- OpenTokSamples/src/com/opentok/android/demo/config/OpenTokConfig.java
+++ OpenTokSamples/src/com/opentok/android/demo/config/OpenTokConfig.java
@@ -5,15 +5,15 @@ public class OpenTokConfig {
 	// *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
 	// ***                      https://dashboard.tokbox.com/projects                           ***
 	// Replace with a generated Session ID
-	public static final String SESSION_ID = "";
+	public static final String SESSION_ID = "2_MX40NDkwNTUxMn5-U2F0IEp1bCAxOSAwNTo1Nzo0MiBQRFQgMjAxNH4wLjY4OTgyMjg1fn4";
 	// Replace with a generated token (from the dashboard or using an OpenTok server SDK)
-	public static final String TOKEN = ""; 
+	public static final String TOKEN = "T1==cGFydG5lcl9pZD00NDkwNTUxMiZzaWc9NDg1MzgwZGFjYzU3NzdlMGM2YTg3NzBlNjg2ZTE0NDUwMDY2NTZkODpyb2xlPXN1YnNjcmliZXImc2Vzc2lvbl9pZD0yX01YNDBORGt3TlRVeE1uNS1VMkYwSUVwMWJDQXhPU0F3TlRvMU56bzBNaUJRUkZRZ01qQXhOSDR3TGpZNE9UZ3lNamcxZm40JmNyZWF0ZV90aW1lPTE0MDU3NzQ2ODQmbm9uY2U9MC4xNjc4MDI4MTA1NDk3MzY3OCZleHBpcmVfdGltZT0xNDA1Nzc4MTk3"; 
 	// Replace with your OpenTok API key
-	public static final String API_KEY= "";
+	public static final String API_KEY= "44905512";
 	
 	// Subscribe to a stream published by this client. Set to false to subscribe
     // to other clients' streams only.
-    public static final boolean SUBSCRIBE_TO_SELF = true;
+    public static final boolean SUBSCRIBE_TO_SELF = false;
   
 	
 }
